bb1bccb27a649caaaf9c8b5abf788d59d30eef00
Kiran Parajuli <kiranparajuli589@gmail.com>
2022-07-04 23:20:42 +0545

    json stringified content assertion for tokenizer index spec
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>

--- tests/unit/tokenizer/index.spec.js
+++ tests/unit/tokenizer/index.spec.js
@@ -6,7 +6,9 @@ describe("tokenization", () => {
   it("should tokenize a markdown content", () => {
     // eslint-disable-next-line no-undef
     const fileContent = read(path.join(__dirname, "markdown.md")).split("\n")
-    expect(tokenize(fileContent)).toMatchSnapshot()
+    // stringified because of \r in raw content of lines
+    // see https://github.com/kiranparajuli589/markdown-parser/runs/7184851840?check_suite_focus=true
+    expect(JSON.stringify(tokenize(fileContent), undefined, 2)).toMatchSnapshot()
   })
 
 })
