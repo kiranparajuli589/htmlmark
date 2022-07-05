90215a05bddaa545e5d1c54bc83963eaec554bb9
Kiran Parajuli <kiranparajuli589@gmail.com>
2022-07-05 23:52:35 +0545

    Added html parser
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>

--- tests/unit/tokenizer/index.spec.js
+++ tests/unit/tokenizer/index.spec.js
@@ -5,9 +5,7 @@ const tokenize = require("../../../lib/tokenizer/index")
 describe("tokenization", () => {
   it("should tokenize a markdown content", () => {
     // eslint-disable-next-line no-undef
-    const fileContent = read(path.join(__dirname, "markdown.md")).split("\n")
-    // stringified because of \r in raw content of lines
-    // see https://github.com/kiranparajuli589/markdown-parser/runs/7184851840?check_suite_focus=true
+    const fileContent = read(path.join(__dirname, "..", "..", "fixtures", "markdown.md")).split("\n")
     expect(tokenize(fileContent)).toMatchSnapshot()
   })
 
