84b60b7c71a2c425756efb5ce08e484c2ab6e164
PKiran <kiranparajuli589@gmail.com>
2022-07-17 12:13:23 +0545

    Upto opening and closing stars
    Signed-off-by: PKiran <kiranparajuli589@gmail.com>

--- check.js
+++ check.js
@@ -1,16 +1,54 @@
-const regex = new RegExp(
-  "`(?<code>[^`]+)`|" +
-  "\\*(?<italic>[^*]+)\\*|" +
-  "\\*\\*(?<bold>(?:(?!\\*\\*).)+)\\*\\*|" +
-  "~~(?<strike>(?:(?!~~).)+)~~|" +
-  "\\[(?<linkTitle>.+)]\\((?<linkHref>.+)\\)|" +
-  "(?<normal>[^`[*~]+)|" +
-  "(?<tara>[*~]{3,})|" +
-  "(?<siTara>[`[]+)",
-  "g"
-)
+const k = "*s **bold** italics*"
 
-const str = "quote [link-title](link-url) with *two*"
+const starBox = []
+let bold = ""
+let italic = ""
+let boldIncomplete = false
+let italicIncomplete = false
+let normal = ""
+const tokens = []
 
+for (let i = 0; i < k.length; i++) {
+  const curr = k[i]
+  const prev = k[i - 1] || null
+  // var next = k[i + 1] || null
 
-console.log(matches)
+  // add consecutive stars to starBox
+  if (curr === "*" && (!prev || prev === "*")) {
+    // if no previous star, add to starBox
+    if (!prev) {
+      starBox.push(curr)
+    } else {
+      const lastStr = starBox.pop()
+      starBox.push(lastStr + curr)
+    }
+  }
+  else if (curr === "*" && prev !== "*") {
+    // Hmm, a new star in the middle of a word
+    // lets check if we have an item in the starBox
+    if (starBox.length >= 1) {
+      // if we have we can check already if that can
+      // be closed with this next star occurrence
+      let cursor = i + 1
+      let mayBeClosingStar = curr
+      // accumulate the next characters until we find a star
+      while (k[cursor] === "*") {
+        mayBeClosingStar += k[cursor]
+        cursor++
+      }
+      if (mayBeClosingStar ===  starBox[starBox.length - 1]) {
+        // if accumulated string is equal to the last entry in the starBox
+        starBox.pop()
+      } else {
+        // otherwise add as a new entry to the starBox
+        starBox.push(mayBeClosingStar)
+      }
+      // set the cursor to the next character
+      i = cursor - 1
+    } else {
+      starBox.push(curr)
+    }
+  }
+}
+
+console.log(starBox)
