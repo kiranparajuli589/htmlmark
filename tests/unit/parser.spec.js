0cc84d7afb8a2b762a62ae1e1bd6d8d987c25193
PKiran <kiranparajuli589@gmail.com>
2022-08-21 18:13:14 +0545

    Added line example with html tags
    Signed-off-by: PKiran <kiranparajuli589@gmail.com>

--- tests/unit/parser.spec.js
+++ tests/unit/parser.spec.js
@@ -465,7 +465,8 @@ describe("Parser", () => {
 		it.each([
 			"some normal and **bold with * gem** but pure *italics* is alos there baby now ~~coming~~hola amigons~~strike~~ wooo lala what about blazing *********here baby ~~~~~~~~~baby `baby```[[[[[[[[[[[[[[[l]]]]int](href)[link](href)``````````",
 			"a paragraph of <u>words</u> `first code` normal text here `code body` *first italics* here me crying *italic body* here me crying **first bolds** some normal again **bold body** [Kiran Parajuli](https://kiranparajuli.com.np) ~~strikes body~~ here some",
-			"now with __underlined text__ within some ___underl_ined_text___"
+			"now with __underlined text__ within some ___underl_ined_text___",
+			"should include html tags inside for <code>code</code> <u>underline</u> <bold>bold</bold> <em>em</em>"
 		])("should be deep tokenized", (line) => {
 			const lines = [line]
 			const lexerData = toHtml(lines)
