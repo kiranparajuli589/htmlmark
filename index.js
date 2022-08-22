ccfba7e8d8c3c63c92dc858ac0c6fc9680143eed
PKiran <kiranparajuli589@gmail.com>
2022-08-22 22:49:03 +0545

    Newline cleanups
    Signed-off-by: PKiran <kiranparajuli589@gmail.com>

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import MDP from "./lib/index.js"
 import { File } from "./lib/util/file.js"
 import { fileURLToPath } from "url"
+import { MDP } from "./lib/index.js"
 
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = File.pathDirname(__filename)
-const htmlContent = MDP.h(File.pathJoin(__dirname, "tests/fixtures/large_markdown.md"))
+const htmlContent = MDP.hF(File.pathJoin(__dirname, "tests/fixtures/large_markdown.md"))
 File.write("./outputs/markdown.html", htmlContent)
