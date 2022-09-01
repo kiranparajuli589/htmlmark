9f75838f3adb2893c0d4f5bce2ccc6e119ea7f46
PKiran <kiranparajuli589@gmail.com>
2022-09-01 17:21:53 +0545

    Add options for demo preview
    Signed-off-by: PKiran <kiranparajuli589@gmail.com>

--- demo/src/App.vue
+++ demo/src/App.vue
@@ -2,22 +2,52 @@
 	<div id="mdp">
 		<nav>
 			<div id="brand">MD Parser</div>
-			<a href="http://github.com/kiranparajuli589/md-parser"
+			<a href="https://github.com/kiranparajuli589/md-parser"
 				 target="_blank" title="GitHub Repository" class="give-a-star"
 			>
 				Give a 🌟
 			</a>
 		</nav>
 		<main>
-			<p>Content Section</p>
+
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;A very lightweight markdown parser.</p>
 			<br>
 
 			<div id="converter">
 				<div class="markdown">
+					<div class="head">
+						<h2 class="section-title">Markdown Input:</h2>
+						<button class="clear" title="Clear Input"
+							@click="clearInput"
+						>
+							❌
+						</button>
+					</div>
 					<textarea name="markdown-input" id="md-input" @input="handleChange" />
 				</div>
 				<div class="o-html">
-					<div class="preview" v-html="htmlContent" />
+					<div class="head">
+						<h2 class="section-title">Output:</h2>
+
+						<select v-model="choice" id="output-choice"
+							title="Choose Output Format"
+						>
+							<option value="preview">Preview</option>
+							<option value="lex">Lexer Data</option>
+							<option value="htmlSource">HTML Source</option>
+						</select>
+
+						<div class="time-taken">⏲️ Elapsed: {{timeTaken}}ms</div>
+					</div>
+					<div class="preview-pane" >
+						<div v-if="choice === 'preview'" class="preview" v-html="output" />
+						<div v-else-if="choice === 'lex'" class="preview">
+							{{ lexerData }}
+						</div>
+						<div v-else class="html-source">
+							{{ output }}
+						</div>
+					</div>
 				</div>
 			</div>
 		</main>
@@ -30,13 +60,25 @@
 import {ref} from "vue"
 import { MDP } from "@kiran/md-parser"
 
-const htmlContent = ref("")
+const output = ref("")
+const timeTaken = ref(0)
+const lexerData = ref({})
+const choice = ref("preview")
 
 const handleChange = (e) => {
-
-	// debounce for 1 second
+	// debounce for 100 ms
 	setTimeout(() => {
-		htmlContent.value = MDP.h(e.target.value.split("\n"))
+		const {elapsedTime, lex, html} = MDP.hP(e.target.value)
+		timeTaken.value = elapsedTime
+		lexerData.value = lex
+		output.value = html
 	}, 100)
+}
+const clearInput = () => {
+	document.getElementById("md-input").value = ""
+	output.value = ""
+	timeTaken.value = 0
+	lexerData.value = {}
+	document.getElementById("md-input").focus()
 }
 </script>
