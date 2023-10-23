cfab678f06c9051a3b7eb5c9ad17ab848f3771d5
Parajuli Kiran <kiranparajuli589@gmail.com>
2023-10-23 23:45:45 +0545

    Lint fixes

--- tests/integration/large.spec.js
+++ tests/integration/large.spec.js
@@ -10,7 +10,7 @@ const mdp = new HtmlMark({
 describe("Large MD To HTML", () => {
 	it.each([
 		// eslint-disable-next-line no-undef
-		File.pathJoin(__dirname, "..", "fixtures/large_markdown.md"),
+		File.pathJoin(__dirname, "..", "fixtures/large_markdown.md")
 	])("should parse the large markdown file content to html", (path) => {
 		const html = mdp.parse(File.read(path))
 		expect(html).toMatchSnapshot()
